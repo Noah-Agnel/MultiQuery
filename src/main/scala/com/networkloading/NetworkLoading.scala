d2808e24684b21a7e6f1bd64157ab58fa4112ee4
Anto188bas <antoniodm_2@msn.com>
2025-07-21 22:57:03 +0200

    fixed bag on no static class

--- src/main/scala/com/networkloading/NetworkLoading.scala
+++ src/main/scala/com/networkloading/NetworkLoading.scala
@@ -1,12 +1,12 @@
 package com.networkloading
 import  org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.col
-import com.sparkconfiguration.SparkHandler
+import com.sparkconfiguration.SparkHandler._
 
 
 object NetworkLoading {    
     def main(args: Array[String]): Unit = {
-       val spark  = SparkHandler.spConfig("Network Loading")
+       val spark  = spConfig("Network Loading")
        
        // Examples of reading files from S3/MinIO buckets
        // Read JSON file from bucket
